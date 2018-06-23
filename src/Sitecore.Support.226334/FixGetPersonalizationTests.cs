87fe22035112dd8bffc284e44bbbf0a403d786f6
Andrey Komarov <akom@sitecore.net>
2018-06-23 12:50:58 -0400

    Original

---
+++ src/Sitecore.Support.226334/FixGetPersonalizationTests.cs
@@ -0,0 +1,26 @@
+namespace Sitecore.Support.ContentTesting.Pipelines.GetTests
+{
+  using Sitecore.ContentTesting.Data;
+  using Sitecore.ContentTesting.Inspectors;
+  using Sitecore.ContentTesting.Pipelines.GetTests;
+  using Sitecore.ContentTesting.Tests;
+  using Sitecore.Diagnostics;
+  using Sitecore.Layouts;
+
+  public class GetPersonalizationTests : Sitecore.ContentTesting.Pipelines.GetTests.GetPersonalizationTests
+  {
+    public override void Process(GetTestsArgs args)
+    {
+      Assert.ArgumentNotNull(args.Item, "args.Item");
+      foreach (RenderingDefinition rendering in new TestingRenderingInspector(args.Item, (SitecoreContentTestStore)null)
+      {
+        InspectionMode = TestingRenderingInspector.Mode.Personalization,
+        DeviceId = args.DeviceId,
+        EnsureTestIsNotRunning = true
+      }.GetRenderings())
+      {        
+          args.AddTest(new PersonalizationTest(rendering, args.Item));       
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.226334/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.226334")]
+[assembly: AssemblyProduct("Sitecore.Support.226334")]
+[assembly: ComVisible(false)]
